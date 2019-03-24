128b4b4ca263221857b213f5769901ce1dd9b2da
Zachary Miller <codebyzach@gmail.com>
2019-03-24 16:29:01 -0400

    repair

--- Terminal.py
+++ Terminal.py
@@ -193,8 +193,8 @@ class OpenTerminalCommand(sublime_plugin.WindowCommand, TerminalCommand):
 
 		self.run_terminal(path, parameters)
 
-    def is_enabled(self):
-        return bool(self.window.active_view() and self.window.active_view().file_name())
+	def is_enabled(self):
+		return bool(self.window.active_view() and self.window.active_view().file_name())
 
 
 class OpenTerminalProjectFolderCommand(sublime_plugin.WindowCommand,
